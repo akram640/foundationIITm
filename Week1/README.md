47ac98dcec88d0a2c75b4099dc75b13c9aa483e7
akram640 <akramfaiz84@gmail.com>
2021-12-16 12:13:46 +0530

    covered some topics

--- Week1/README.md
+++ Week1/README.md
@@ -1,4 +1,5 @@
 Topic
 
 1. A Quick Introduction to Variable
-
+2. Variables and Input Statement
+3. Variables and Literals
---
+++ Week1/main.py
@@ -0,0 +1,9 @@
+print("Hello, type in your name: ")
+n = str(input())
+print("Which place are you in ?")
+p = str(input())
+print("Hello",n,"How is the weather in",p,"?")
+print("What is your age?")
+age = int(input())
+print("Good to know you are",age,"years old")
+
