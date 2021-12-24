511d10ddbde6fa40b581e0cac9bad0b087674a44
akram640 <akramfaiz84@gmail.com>
2021-12-24 09:53:16 +0530

    week3 start

---
+++ Week3/README.md
@@ -0,0 +1,3 @@
+1. While loop
+
+2. 
---
+++ Week3/main.py
@@ -0,0 +1,9 @@
+print("When did India get its independence ? ")
+year = int(input())
+
+while(year != 1947):
+    print("You got this wrong. Enter once again.")
+    year = int(input())
+
+print("Wowwwwwwww... You got it right Hurrey")
+
