1fa2f2aa38d6949289dec9defd3108234d1abe3f
Akram Faiz <akramfaiz84@gmail.com>
2022-03-12 08:50:52 +0530

    l11-3 complt

--- PythonCourse/week11/main.py
+++ PythonCourse/week11/main.py
@@ -1,4 +1,13 @@
-fruits = ['mango','apple','banana','orange']
+def square(limit):
+    x = 0
+    while x < limit:
+        yield x * x
+        yield x * x * x
+        x += 1
 
-for fruit in fruits:
-    print(fruit)
+a = square(5)
+print(next(a),next(a))
+print(next(a),next(a))
+print(next(a),next(a))
+print(next(a),next(a))
+print(next(a),next(a))
